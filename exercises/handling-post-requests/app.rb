6e38224b3ddb6b3d77663c2c9c46c4975b4ca2e3
Mark Goldenson <mjgold1@gmail.com>
2015-02-05 01:53:12 -0800

    Initial commit, broken post recipe method

--- exercises/handling-post-requests/app.rb
+++ exercises/handling-post-requests/app.rb
@@ -6,6 +6,20 @@ get '/' do
   erb :recipes
 end
 
+post '/recipes' do
+  puts params
+  # title = params[:title]
+  # created_by = params[:created_by]
+  # description = params[:description]
+  # instructions = params[:instructions]
+
+  Recipe.create(title: params[:title],
+                created_by: params[:created_by],
+                description: params[:description],
+                instructions: params[:instructions])
+  erb :recipes
+end
+
 get '/recipes/:recipe_id' do
   if @recipe = Recipe.get(params[:recipe_id])
     erb :show_recipe
@@ -16,4 +30,3 @@ get '/recipes/:recipe_id' do
     # http://www.sinatrarb.com/intro.html#Return%20Values
   end
 end
-
